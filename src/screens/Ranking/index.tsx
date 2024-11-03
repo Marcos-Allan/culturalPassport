d36f11f393731ce7faebd4ff2a561d9e1c84d353
Marcos Allan <allanmenezes880@gmail.com>
2024-11-03 17:49:26 -0300

    editei a tela de ranking

--- src/screens/Ranking/index.tsx
+++ src/screens/Ranking/index.tsx
@@ -110,12 +110,52 @@ export default function Ranking() {
                 <MenuButton />
             </Navbar>
 
-            <div className={`w-[90%] sm:px-12 sm:w-[70%] mb-[100px] sm:mb-[40px] lg:mb-0 flex items-center flex-col overflow-y-scroll scrollbar scrollbar-track-transparent scrollbar-thumb-my-secondary`}>
+
+            <div className={`w-[90%] sm:ml-[30px] sm:px-12 sm:w-[70%] mb-[100px] sm:mb-[40px] lg:mb-0 flex items-center flex-col overflow-y-scroll scrollbar scrollbar-track-transparent scrollbar-thumb-my-secondary`}>
+                
+                <div className={`w-[90%] flex justify-center items-end px-12 lg:px-32 my-4`}>
+                    {loadingContent == false && users.length > 0 && users.map((user, i) => (
+                        <>
+                            {i == 0 && (
+                                <div className='flex-grow-[1] h-[200px] bg-my-quartenary relative flex items-center justify-center order-1 rounded-t-[8px]'>
+                                    <div className={`mt-2 absolute top-0`}>
+                                        <img src={user.img} className={`w-[40px] rounded-[50%] border-[2px] mb-1 animate-colorChange`} />
+                                        <p className={`text-center font-bold text-my-white`}>1°</p>
+                                    </div>
+                                </div>
+                            )}
+                            {i == 1 && (
+                                <div className='flex-grow-[1] h-[150px] bg-my-terciary relative flex items-center justify-center order-2 rounded-tr-[8px]'>
+                                    <div className={`mt-2 absolute top-0`}>
+                                        <img src={user.img} className={`w-[40px] rounded-[50%] border-[2px] border-transparent mb-1`} />
+                                        <p className={`text-center font-bold text-my-white`}>2°</p>
+                                    </div>
+                                </div>
+                            )}
+                            {i == 2 && (
+                                <div className='flex-grow-[1] h-[120px] bg-my-secondary relative flex items-center justify-center order-0 rounded-tl-[8px]'>
+                                    <div className={`mt-2 absolute top-0`}>
+                                        <img src={user.img} className={`w-[40px] rounded-[50%] border-[2px] border-transparent mb-1`} />
+                                        <p className={`text-center font-bold text-my-white`}>3°</p>
+                                    </div>
+                                </div>
+                            )}
+                        </>
+                    ))}
+                </div>
+
                 {loadingContent == false && users.length > 0 && users.map((user, i) => (
-                    <div className={`w-[95%] flex justify-between items-center mb-6 ${theme == 'light' ? 'text-my-black' : 'text-my-white'}`}>
+                    <div className={`
+                        w-[95%] flex justify-between items-center mb-2 text-my-white
+                        ${i == 0 && 'bg-my-quartenary'}
+                        ${i == 1 && 'bg-my-terciary'}
+                        ${i == 2 && 'bg-my-secondary'}
+                        ${i >= 3 && 'bg-my-gray'}
+                        px-4 rounded-[6px] py-2
+                    `}>
                         <div className={`flex items-center justify-start gap-1`}>
                             <p className={`mr-3`}>{i+1}°</p>
-                            <img src={user.img} className={`w-[80px] h-[80px] rounded-[50%] border-[6px] ${i == 0 ? 'animate-colorChange' : 'border-transparent'}`} />
+                            <img src={user.img} className={`w-[80px] h-[80px] rounded-[50%] border-[6px] ${i == 0 ? 'border-transparent' : 'border-transparent'}`} />
                             <p>{user.name}</p>
                         </div>
                         <p>{user.simulationsConcludeds}</p>
